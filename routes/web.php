7cee5d76c913bfb44c5cb26418cd02d8df285d6d
Hanis <hanieschieynaf@gmail.com>
2022-10-07 18:06:34 +0800

    c

--- routes/web.php
+++ routes/web.php
@@ -207,3 +207,6 @@ Route::get('/timesheet/timesheetovertimereport', function () {
 Route::get('/setting/timesheetperiod', function () {
     return view('settingtimesheetperiod');
 });
+Route::get('/report/reportingclaimreport', function () {
+    return view('reportingclaimreport');
+});
